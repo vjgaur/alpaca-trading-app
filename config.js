edd0dc2b1698de27b278cc61a7f238d105b0cde4
vjgaur <vijayendra.gaur@gmail.com>
2022-02-01 13:40:55 +0530

    nothing

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    ALPACA_API_KEY_ID: "PK0E2RU2NKRFULY633WA",
-    ALPACA_API_SECRET_KEY: "Ndb6oWW0K12ScznmtefnNadsFDjXhcHk2QuajJy4",
+    ALPACA_API_KEY_ID: "use your alpaca api key id",
+    ALPACA_API_SECRET_KEY: "use your alpaca api secret key ",
     ALPACA_URL: "https://papper-api.alpaca.markets/",
     POLYGON_URL: "https://polygon.io"
 
